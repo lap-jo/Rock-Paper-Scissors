0ebf14f3f882df6f9b281aac559e2e4131594ea2
lap-jo <okelloj219@gmail.com>
2022-10-03 19:03:29 +0300

    adding function to script.js to get computerChoice and to playRound announcing the winner

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('hello')
+function getComputerChoice(computerOptions) {
+    computerOptions = ["rock", "paper", "scissors"];
+    return computerOptions[Math.floor(Math.random() * computerOptions.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("rock, paper, or scissors", "");
+    computerSelection = "paper";
+
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return `It's a tie`;
+    } else if (
+        playerSelection.toLowerCase() === "rock" &&
+        computerSelection === "scissors"
+    ) {
+        return "You Win this round";
+    } else if (
+        playerSelection.toLowerCase() === "scissors" &&
+        computerSelection === "paper"
+    ) {
+        return "You Win this round";
+    } else if (
+        playerSelection.toLowerCase() === "paper" &&
+        computerSelection === "rock"
+    ) {
+        return "You Win this round";
+    } 
+    return "You Lose"
+}
+console.log(playRound());
