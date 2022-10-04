571c88a3287e0c1d1d46571c0a6ae309c0559be1
lap-jo <okelloj219@gmail.com>
2022-10-04 11:56:15 +0300

    adding storage for scores to script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+//function gives the computer a choice from (Rock, Paper, or Scissors) and lets
+//it choose one and return a random one.
 function getComputerChoice(computerOptions) {
     computerOptions = ["rock", "paper", "scissors"];
     return computerOptions[Math.floor(Math.random() * computerOptions.length)];
 }
 
+function getPlayerChoice() {
+    let input = prompt("rock, paper, or scissors", "");
+    return input.toLowerCase();
+}
+
+let playerScore = 0;
+let computerScore = 0;
+// Function plays a single round of the game.
+// Prompt the player for input once thats done the computer plays
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("rock, paper, or scissors", "");
-    computerSelection = "paper";
+    // playerSelection = prompt("rock, paper, or scissors", "");
+    // computerSelection = getComputerChoice();
 
     if (playerSelection.toLowerCase() === computerSelection) {
         return `It's a tie`;
@@ -13,18 +24,30 @@ function playRound(playerSelection, computerSelection) {
         playerSelection.toLowerCase() === "rock" &&
         computerSelection === "scissors"
     ) {
+        playerScore++;
         return "You Win this round";
     } else if (
         playerSelection.toLowerCase() === "scissors" &&
         computerSelection === "paper"
     ) {
+        playerScore++;
         return "You Win this round";
     } else if (
         playerSelection.toLowerCase() === "paper" &&
         computerSelection === "rock"
     ) {
+        playerScore++;
         return "You Win this round";
-    } 
-    return "You Lose"
+    }
+    computerScore++;
+    return "You Lose";
 }
-console.log(playRound());
+
+function game() {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`Player: ${playerScore} Computer:  ${computerScore}`);
+}
+
+console.log(game());
